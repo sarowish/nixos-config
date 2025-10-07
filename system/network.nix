4bbe7df74ae637454676b9484a6ff56653eef525
Berke Enercan <berkeenercan@tutanota.com>
2025-10-07 21:22:06 +0300

    use static ip

--- system/network.nix
+++ system/network.nix
@@ -1,13 +1,18 @@
-{ pkgs, ... }:
-
 {
   networking.hostName = "be";
-
-  systemd.network = {
-    networks."enp34s0" = {
-      matchConfig.name = "enp34s0";
-      networkConfig.DHCP = "ipv4";
-    };
+  networking.useDHCP = false;
+  networking.useNetworkd = true;
+  networking.interfaces.enp34s0 = {
+    ipv4.addresses = [
+      {
+        address = "192.168.1.130";
+        prefixLength = 24;
+      }
+    ];
+  };
+  networking.defaultGateway = {
+    address = "192.168.1.1";
+    interface = "enp34s0";
   };
 
   services.resolved.enable = true;
