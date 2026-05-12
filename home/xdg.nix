1158d74d7a8ca3ad30b168550487a264d77ab98a
Berke Enercan <berkeenercan@tutanota.com>
2026-05-12 20:51:47 +0300

    feat: set librewolf as the default browser

--- home/xdg.nix
+++ home/xdg.nix
@@ -8,6 +8,18 @@
     enable = true;
     mime.enable = true;
 
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "default-web-browser" = [ "librewolf.desktop" ];
+        "text/html" = [ "librewolf.desktop" ];
+        "x-scheme-handler/http" = [ "librewolf.desktop" ];
+        "x-scheme-handler/https" = [ "librewolf.desktop" ];
+        "x-scheme-handler/about" = [ "librewolf.desktop" ];
+        "x-scheme-handler/unknown" = [ "librewolf.desktop" ];
+      };
+    };
+
     portal = {
       enable = true;
       extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
