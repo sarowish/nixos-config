5873c8fe7000c632b71ede54ed8179b680595b2c
Berke Enercan <berkeenercan@tutanota.com>
2026-06-01 17:39:46 +0300

    feat(mpd-herald): remove config

--- home/mpd.nix
+++ home/mpd.nix
@@ -22,12 +22,5 @@
   services.mpd-herald = {
     enable = true;
     package = inputs.mpd-herald.packages.${pkgs.stdenv.hostPlatform.system}.default;
-    settings = {
-      scrobbling.lastfm = {
-        enable = true;
-        api_key = "f29aa97582fb204d125eeaa93df2ea5d";
-        secret = "ae86e05c49bafbd7c4a3d017ed192bb5";
-      };
-    };
   };
 }
