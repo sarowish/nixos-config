cec23dfa409a3268a02ac47f4d7f6385459d07b7
Berke Enercan <berkeenercan@tutanota.com>
2026-08-09 21:52:26 +0300

    feat: launch niri on startup

--- system/greetd.nix
+++ system/greetd.nix
@@ -8,9 +8,9 @@ in
     enable = true;
 
     settings = {
-      default_session.command = "${tuigreet} --time --remember --asterisks --cmd start-hyprland";
+      default_session.command = "${tuigreet} --time --remember --asterisks --cmd niri-session";
       initial_session = {
-        command = "start-hyprland";
+        command = "niri-session";
         user = "chels";
       };
     };
